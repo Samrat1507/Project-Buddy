e2d7eb1271ef30264caf938307adedd72efff411
Samrat1507 <lazyworm3325@gmail.com>
2024-02-08 10:24:09 +0530

    routing

--- ProjectBuddy-main/src/pages/components/Navbar.js
+++ ProjectBuddy-main/src/pages/components/Navbar.js
@@ -1,37 +1,43 @@
-import React from 'react'
-import Switcher from './Switcher'
-import { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import Switcher from './Switcher';
 import useDarkSide from '@/hooks/useDarkSide';
-
+import Link from 'next/link';
 
 export const Navbar = () => {
-
   useEffect(() => {
-    if(!localStorage.getItem("theme"))
-      localStorage.setItem("theme", "light");
+    if (!localStorage.getItem('theme')) localStorage.setItem('theme', 'light');
   }, []);
 
   const isDarkMode = useDarkSide();
+  console.log(isDarkMode);
 
   return (
-    <div className='bg-blue/80 h-fit w-full text-xl rounded-xl shadow-md dark:bg-otherWhite/20'>
-        <ul className='flex justify-between px-10 py-5'>
-            <div className='flex gap-10'>
+    <div className="bg-blue/80 h-fit w-full text-xl rounded-xl shadow-md dark:bg-otherWhite/20">
+      <ul className="flex justify-between px-10 py-5">
+        <div className="flex gap-10">
+          <Link href="/">
             <img
-            src={isDarkMode ? "LogoDark.svg" : "Logo.svg"} // Conditionally rendering the logo based on the mode
-            alt={isDarkMode ? "logo_dark" : "logo"}
-            className="h-12"
-          />
-              
-            </div>
-            <div className='flex gap-10 items-center dark:text-otherWhite'>
-            <li className=''>About</li>
-            <li className=''>Login</li>
-            <li className=''>SignUp</li>
-            {/* <li className=''>ModeButton</li> */}
-            <li><Switcher/></li>
-            </div>
-        </ul>
+              src={isDarkMode ? '/LogoDark.svg' : '/Logo.svg'}
+              alt={isDarkMode ? 'logo_dark' : 'logo'}
+              className="h-12"
+            />
+          </Link>
+        </div>
+        <div className="flex gap-10 items-center dark:text-otherWhite">
+          <li>
+            <Link href="/About">About</Link>
+          </li>
+          <li>
+            <Link href="/Login">Login</Link>
+          </li>
+          <li>
+            <Link href="/SignUp">SignUp</Link>
+          </li>
+          <li>
+            <Switcher />
+          </li>
+        </div>
+      </ul>
     </div>
-  )
-}
+  );
+};
