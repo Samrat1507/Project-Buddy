d1990c016c5c2a60a03cef7561ffac14e5d4591c
Annnesha <annesha250903@gmail.com>
2024-02-08 15:27:47 +0530

    Login dark Mode

--- ProjectBuddy-main/src/pages/Login.js
+++ ProjectBuddy-main/src/pages/Login.js
@@ -7,20 +7,20 @@ const Login = () => {
         {/* <div> */}
             <Navbar />
         {/* </div> */}
-        <div className="flex flex-col bg-otherBlue gap-10 items-center w-1/4 py-20 rounded-xl">
-            <h1 className='text-6xl'>Login</h1>
-            <input className='bg-lightBlue placeholder-black/60 px-5 py-1 rounded-md'
+        <div className="flex flex-col bg-otherBlue gap-10 items-center w-1/4 py-20 rounded-xl dark:bg-otherWhite/20">
+            <h1 className='text-6xl dark:text-otherWhite'>Login</h1>
+            <input className='bg-lightBlue shadow-md shadow-gray-500 dark:shadow-black/80 placeholder-black/60 px-5 py-1 rounded-md dark:bg-black/40 dark:placeholder-otherWhite/60'
                 type="text"
                 placeholder="Username"
             />
-            <div className='flex flex-col'>
-                <input className='bg-lightBlue placeholder-black/60 px-5 py-1 rounded-md'
+            <div className='flex flex-col gap-1'>
+                <input className='bg-lightBlue shadow-md shadow-gray-500 dark:shadow-black/80 placeholder-black/60 px-5 py-1 rounded-md dark:bg-black/40 dark:placeholder-otherWhite/60'
                     type="text"
                     placeholder="Password"
                 />  
-                <span className='text-otherWhite text-xs text-'>Forgot Password?</span>
+                <span className='text-otherWhite text-xs dark:text-black/80 text-'>Forgot Password?</span>
             </div>                
-            <button className=" bg-blue/80 px-8 rounded-md shadow-md w-fit text-white shadow-gray-500">
+            <button className=" bg-blue/80 px-8 rounded-md shadow-md w-fit text-white shadow-gray-500 dark:bg-black/60 dark:shadow-black/80">
                 Submit
             </button>            
         </div>
